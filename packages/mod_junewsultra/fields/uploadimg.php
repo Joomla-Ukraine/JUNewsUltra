59da2b6546791b681611629a77fb6628772a213b
Den <dgm.denys@gmail.com>
2020-03-14 19:02:57 +0200

    Fix upload image

--- packages/mod_junewsultra/fields/uploadimg.php
+++ packages/mod_junewsultra/fields/uploadimg.php
@@ -86,11 +86,8 @@ function alert($text, $error)
 	return;
 endif;
 
-$path             = str_replace('modules' . DS . 'mod_junewsultra' . DS . 'fields' . DS . '..' . DS . '..' . DS . '..', 'media/mod_junewsultra', JPATH_BASE);
-$max_image_width  = 800;
-$max_image_height = 800;
-$max_image_size   = 1024 * 1024;
-$valid_types      = [
+$path        = str_replace('modules' . DS . 'mod_junewsultra' . DS . 'fields' . DS . '..' . DS . '..' . DS . '..', 'media/mod_junewsultra', JPATH_BASE);
+$valid_types = [
 	'gif',
 	'jpg',
 	'png',
@@ -113,7 +110,6 @@ $valid_types      = [
                <input id="lefile" name="userfile" type="file" autocomplete="off">
                <button type="submit" class="btn btn-primary"><?php echo JText::_('MOD_JUNEWS_UPLOAD'); ?></button>
             </span>
-		<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_image_size; ?>">
 	</form>
 </fieldset>
 <?php
@@ -124,31 +120,20 @@ if(isset($_FILES[ 'userfile' ]))
 		$filename = $_FILES[ 'userfile' ][ 'tmp_name' ];
 		$ext      = substr($_FILES[ 'userfile' ][ 'name' ], 1 + strrpos($_FILES[ 'userfile' ][ 'name' ], '.'));
 
-		if(filesize($filename) > $max_image_size)
-		{
-			echo alert(JText::_('MOD_JUNEWS_ERROR1') . $max_image_size . ' KB', 'notice');
-		}
-		elseif(!in_array($ext, $valid_types))
+		if(!in_array($ext, $valid_types))
 		{
 			echo alert(JText::_('MOD_JUNEWS_ERROR2'), 'notice');
 		}
 		else
 		{
 			$size = getimagesize($filename);
-			if($size && ($size[ 0 ] < $max_image_width) && ($size[ 1 ] < $max_image_height))
+			if(@move_uploaded_file($filename, $path . '/jn_' . $_FILES[ 'userfile' ][ 'name' ]))
 			{
-				if(@move_uploaded_file($filename, $path . '/jn_' . $_FILES[ 'userfile' ][ 'name' ]))
-				{
-					echo alert(JText::_('MOD_JUNEWS_NOTICE8'), 'message');
-				}
-				else
-				{
-					echo alert(JText::_('MOD_JUNEWS_ERROR3'), 'notice');
-				}
+				echo alert(JText::_('MOD_JUNEWS_NOTICE8'), 'message');
 			}
 			else
 			{
-				echo alert(JText::_('MOD_JUNEWS_ERROR4'), 'notice');
+				echo alert(JText::_('MOD_JUNEWS_ERROR3'), 'notice');
 			}
 		}
 	}
